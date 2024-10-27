3c6d546dfc6f9099148d8392069e9753d2e032f4
rrrgsrssingh-saurav <sauravlkr69@gmail.com>
2024-10-27 23:37:20 +0530

    React BY akshay-saini

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+const heading=React.createElement("h1",{id:"heading"},"Hello Saurav kumar singh nadiyawan");
+const root=ReactDom.createRoot(document.getElementById("root"));
+root.render(heading);
